7f320cb0e119b5e498d2d8679a5702ffbcf2c380
levanidus <levanidus@gmail.com>
2022-04-20 21:29:53 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"levanidus/todo-app"
+	"levanidus/todo-app/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
