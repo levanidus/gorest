2e4cdec7b52e6c9387042072ec0b63e3857f6201
levanidus <levanidus@gmail.com>
2022-04-27 15:43:50 +0300

    Creating jwt token

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -5,9 +5,21 @@ import (
 	"fmt"
 	"levanidus/todo-app"
 	"levanidus/todo-app/pkg/repository"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "jghsahldghgdl"
+const (
+	salt      = "jghsahldghgdl"
+	tokenTTL  = 12 * time.Hour
+	signinKey = "#677768786gfghh#k"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +32,26 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	},
+	)
+
+	return token.SignedString([]byte(signinKey))
+
 }
 
 func generatePasswordHash(password string) string {
