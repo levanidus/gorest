95251bdbadd4680036ff97f457b9ab46a28efce3
levanidus <levanidus@gmail.com>
2022-05-13 14:43:02 +0300

    Update item feature

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -24,6 +24,7 @@ type TodoItem interface {
 	GetAll(userId, listId int) ([]todo.TodoItem, error)
 	GetById(userId, itemId int) (todo.TodoItem, error)
 	Delete(userId, itemId int) error
+	Update(userId, listId int, input todo.UpdateItemInput) error
 }
 
 type Repository struct {
